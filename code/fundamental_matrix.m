e1478ca063e5dc24bf1c621f2fcdc620f08fdb37
Neubotech <neubotech@gmail.com>
2012-09-26 15:12:59 -0700

    updated two files

--- code/fundamental_matrix.m
+++ code/fundamental_matrix.m
@@ -5,12 +5,12 @@ function [F res_err] = fundamental_matrix(Matrix)
 % %     x1=Matrix(:,1:2);
 % %     x2=Matrix(:,3:4);
 % %     
-% %     T_scale1=sqrt(sum(var(x1,1)));
-% %     T_scale2=sqrt(sum(var(x2,1)));
+% %     T_scale1=sqrt(2./sum(var(x1,1)));
+% %     T_scale2=sqrt(2./sum(var(x2,1)));
 % %     
 % %     for i=1:2
-% %         x1(:,i)=(x1(:,i)-T_mean(i))./(T_scale1);
-% %         x2(:,i)=(x2(:,i)-T_mean(i))./(T_scale2);
+% %         x1(:,i)=(x1(:,i)-T_mean(i)).*2./(T_scale1);
+% %         x2(:,i)=(x2(:,i)-T_mean(i)).*2./(T_scale2);
 % %     end
         
     %% Homogeneious Coordinate
@@ -18,15 +18,15 @@ function [F res_err] = fundamental_matrix(Matrix)
     X2=[Matrix(:,3) Matrix(:,4) ones(size(Matrix(:,3)))]';
     
     
-    %% Inverse of T1 & T2
+    %%  T1 & T2
     T_mean=mean(Matrix,1);
     T_var=var(Matrix,1);
-    T_std1=sqrt(sum(T_var(1:2)));
-    T_std2=sqrt(sum(T_var(3:4)));
-    T1=diag(1./[T_std1 T_std1])*[eye(2) -T_mean(1:2)']; %translate, then scale
-    %T1=[T1; 0 0 1];
-    T2=diag(1./[T_std2 T_std2])*[eye(2) -T_mean(3:4)'];
-    %T2=[T2; 0 0 1];
+    T_scale1=sqrt(2/sum(T_var(1:2)));
+    T_scale2=sqrt(2/sum(T_var(3:4)));
+    T1=T_scale1.*[eye(2) -T_mean(1:2)']; %translate, then scale
+    T1=[T1; 0 0 1];
+    T2=T_scale2.*[eye(2) -T_mean(3:4)'];
+    T2=[T2; 0 0 1];
             
     x1=T1*X1; %normalized x1 x2
     x2=T2*X2;
@@ -36,27 +36,20 @@ function [F res_err] = fundamental_matrix(Matrix)
     A=[x1(:,1).*x2(:,1) x1(:,2).*x2(:,1) x2(:,1) x1(:,1).*x2(:,2) x1(:,2).*x2(:,2) x2(:,2) x1(:,1) x1(:,2) ones(size(x1(:,1)))];
     
      %% equation 6&7
-    [U1 S1 V1]=svd(A);
+    [U1 S1 V1]=svd(A,0);
     
     %% formulate F*, ??? check if it should be col or row vectors in reconstruct
-    F=V1(end,:);
-    F=[F(1:3);F(4:6);F(7:9)];
+    F=V1(:,end);
+    F=[F(1:3)';F(4:6)';F(7:9)'];
     
     %% Equation 8
     [U2 S2 V2]=svd(F);
     S2(end,end)=0;
     F=U2*S2*V2';
     
-    %% T1 & T2
-    
-    T1_=[eye(2) T_mean(1:2)']*(diag([T_std1 T_std1 1]));
-    T1_=[T1_;0 0 1];
-    T2_=[eye(2) T_mean(3:4)']*(diag([T_std2 T_std2 1]));
-    T2_=[T2_;0 0 1];
-    
     
     %% un-normalize ???check transpose ...
-    F=T2_'*F*T1_;
+    F=T2'*F*T1;
     
     %% residual, calculation
     Square=(X1-X2).^2;
